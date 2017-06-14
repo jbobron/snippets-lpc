a49594879c9fa79643fac199761c3791aee814d3
Jake Obron <jbobron@gmail.com>
2017-06-14 14:34:37 -0700

    first commit

---
+++ spec/lpc-frontend-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LpcFrontendSnippetsView from '../lib/lpc-frontend-snippets-view';
+
+describe('LpcFrontendSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
